ef983f28ee207489e5b94add48e8e6e3b023e1ff
Gozde <gozde.a.997@gmail.com>
2024-09-02 12:51:53 +0300

    Initial Commit

---
+++ Hacker News 5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hacker News 5
+//
+//  Created by Gözde Aydin on 2.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hacker News 5/Hacker_News_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_5App.swift
+//  Hacker News 5
+//
+//  Created by Gözde Aydin on 2.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
